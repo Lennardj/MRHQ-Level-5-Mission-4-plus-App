d1e80312e66c970d418003e17eed68ddb37472d0
Lennardj <lennardvincentjohn@gmail.com>
2022-12-15 11:46:38 +1300

    addes axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+const axios = require("axios").default;
 
 function App() {
+  const [state, setState] = useState({
+    name: "",
+    risk: "",
+  });
+  function handleChange(e) {
+    // console.log(e.target.value);
+    // setState({ Name: e.target.value, risk: e.target.value });
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
+    console.log(state);
+  }
+  async function handleSubmit(e) {
+    fetch(
+      "https://riskrating.azurewebsites.net/api/riskRatingApi?code=Ur7ZxfXwicGfa3Gfhqv9RLCipQev2VyDMTUTxuGmFd6QAzFuPunD7g==",
+      { Method: "POST" }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    // try {
+    //   axios
+    //     .post("https://riskrating.azurewebsites.net/api/riskRatingApi?", {
+    //       name: state.name,
+    //       risk: state.risk,
+    //     })
+    //     .then(function (response) {
+    //       console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Risk Rating</h1>
+      <div>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+            value={state.name}
+          />
+        </div>
+        <label>Describe Your claim history for the last 3 years</label>
+        <br />
+        <br />
+        <textarea
+          rows="4"
+          cols="50"
+          type="text"
+          id="risk"
+          name="risk"
+          placeholder="please discribe you history for the last 3 years"
+          onChange={handleChange}
+          value={state.risk}
+        ></textarea>
+        <br />
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+
+      <div id="answer">
+        <p>Answer will be displayed here</p>
+      </div>
     </div>
   );
 }
